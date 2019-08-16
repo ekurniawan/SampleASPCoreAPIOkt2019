7d4fcfe94ee9ccd4d3fa3c00da922cd1b234bf79
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-08-16 14:57:33 +0700

    menambahkan httpclient

--- SampleASPCore/Controllers/HelloController.cs
+++ SampleASPCore/Controllers/HelloController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Newtonsoft.Json;
+using SampleASPCore.Models;
 
 namespace SampleASPCore.Controllers
 {
     public class HelloController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            HttpClient _client = new HttpClient();
+            _client.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImFkbWluQGdtYWlsLmNvbSIsIm5iZiI6MTU2NTk0MTAwNSwiZXhwIjoxNTY1OTYyNjA1LCJpYXQiOjE1NjU5NDEwMDV9.D3meoBIwIYqJArqPaX2IVqUDku9PWjtneI5h4erZ5zQ");
+            HttpResponseMessage response = await _client.GetAsync("https://localhost:44338/api/Barang");
+            var content = await response.Content.ReadAsStringAsync();
+            var results = JsonConvert.DeserializeObject<List<Barang>>(content);
+
+            return View(results);
         }
 
         public IActionResult About()
---
+++ SampleASPCore/Models/Barang.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleASPCore.Models
+{
+    public class Barang
+    {
+        public string KodeBarang { get; set; }
+        public string NamaBarang { get; set; }
+        public int Jumlah { get; set; }
+        public decimal HargaBeli { get; set; }
+        public decimal HargaJual { get; set; }
+        public string Deskripsi { get; set; }
+    }
+}
